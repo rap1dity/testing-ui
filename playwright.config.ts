7f4b2198f31caf5086f5459935592799049a0e99
m.tsishchykau <m.tishchykau@besmart.by>
2025-05-29 00:12:11 +0300

    chore(TL-2): Added another device in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,14 +7,23 @@ export default defineConfig({
     headless: false,
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
-    viewport: { width: 1920, height: 1080 },
   },
   retries: 1,
   reporter: [['html', { open: 'never' }]],
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' }
+      name: 'chromium-1080p',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: 'firefox-768p',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 1366, height: 768 },
+      },
     },
   ],
 });
