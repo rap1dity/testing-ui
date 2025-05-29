8addd8cd67d1afdfe7c2ca5c636e841695ce3402
Andrew <Konwertik1@mail.ru>
2025-05-29 22:17:38 +0200

    refactor(TL-4)

--- src/pages/alerts.page.ts
+++ src/pages/alerts.page.ts
@@ -4,6 +4,8 @@ import { AlertButton } from '@common/types/alert-button.type';
 
 export class AlertsPage extends BasePage {
   private readonly buttonSelectors: Record<AlertButton, Locator>;
+  private readonly confirmResult: Locator;
+  private readonly promptResult: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -14,6 +16,9 @@ export class AlertsPage extends BasePage {
       confirmButton: page.locator('#confirmButton'),
       promptButton: page.locator('#promtButton'),
     };
+
+    this.confirmResult = page.locator('#confirmResult');
+    this.promptResult = page.locator('#promptResult');
   }
 
   async openPage(): Promise<void> {
@@ -24,5 +29,13 @@ export class AlertsPage extends BasePage {
     await this.buttonSelectors[button].waitFor({ state: 'visible' });
 
     await this.buttonSelectors[button].click();
+  }
+
+  async getConfirmResult(): Promise<string> {
+    return this.confirmResult.innerText();
+  }
+
+  async getPromptResult(): Promise<string> {
+    return this.promptResult.innerText();
   }
 }
